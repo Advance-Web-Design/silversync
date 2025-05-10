3086ddf75be905170aea3094a3300fccaaca29d4
ishay <ishay.yulzary@gmail.com>
2025-05-10 17:35:09 +0300

    feat: Remove LoadingOverlay from GameplayArea; update context usage for cleaner code

--- client/src/components/game/GameplayArea.jsx
+++ client/src/components/game/GameplayArea.jsx
@@ -4,20 +4,17 @@ import { Box } from '@mui/material';
 import GameBoard from './GameBoard';
 import SearchPanel from './SearchPanel';
 import VictoryModal from './VictoryModal';
-import LoadingOverlay from './LoadingOverlay';
 import BoardHeader from './BoardHeader';
 import { useGameContext } from '../../contexts/gameContext';
 
 function GameplayArea() {
-  const { gameCompleted, keepPlayingAfterWin, isLoading } = useGameContext();
+  const { gameCompleted, keepPlayingAfterWin } = useGameContext();
 
   return (
     <Box className="relative flex-1 flex flex-col">
       <BoardHeader /> 
       
       <GameBoard />
-      
-      {isLoading && <LoadingOverlay />} {/* Add LoadingOverlay here */}
 
       {/* SearchPanel is rendered outside the flex layout as a fixed element */}
       <SearchPanel />
