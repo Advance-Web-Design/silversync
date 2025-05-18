c980e2d44de572af5728322b5033e1bf7ccae413
NathTro <tros1009@gmail.com>
2025-05-18 13:08:27 +0300

    added include for tailwind in index.css and made tailwind design for HowToPlay.jsx

--- client/src/components/HowToPlay.jsx
+++ client/src/components/HowToPlay.jsx
@@ -12,12 +12,12 @@ function HowToPlay() {
   return (
     <>
       {isOpen && (
-        <div className="popup-overlay">
-          <div className="popup-content">
-            <button className="popup-close-button" onClick={handleClose}>
+        <div className="fixed top-0 left-0 right-0 bottom-0 bg-black/50 flex justify-center items-center z-[1000]">
+          <div className="bg-white p-5 rounded-lg shadow-lg relative w-4/5 max-w-lgb h-1/5 border-4 border-[#9ab0c3]">
+            <button className="absolute top-[10px] right-[10px] bg-transparent border-none cursor-pointer text-[#666] hover:text-black" onClick={handleClose}>
               <CloseIcon />
             </button>
-            <div className="popup-body">
+            <div className="mt-5 text-center">
               <h2>How to Play</h2>
               <p>Welcome to Connect the Shows! This game challenges you to find connections between TV shows and movies through actors.</p>
               <p>1. Select actors that connect different shows</p>
