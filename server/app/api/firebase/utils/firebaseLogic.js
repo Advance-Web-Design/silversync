d4ec69a44b65cac69a26ba438478549b798f01b2
VictorRyskinB <victor.ryskin@e.braude.ac.il>
2025-05-30 01:31:14 +0300

    removed apikeys in .js file. used .env

--- server/app/api/firebase/utils/firebaseLogic.js
+++ server/app/api/firebase/utils/firebaseLogic.js
@@ -10,26 +10,17 @@ import { getAnalytics } from "firebase/analytics";
 
 
 
+
 const firebaseConfig = {
-
-  apiKey: "AIzaSyBDxmC4dD1h5rSX4B0fYXdXyrfVkfRQfVA",
-
-  authDomain: "connect-the-shows.firebaseapp.com",
-
-  databaseURL: "https://connect-the-shows-default-rtdb.europe-west1.firebasedatabase.app",
-
-  projectId: "connect-the-shows",
-
-  storageBucket: "connect-the-shows.firebasestorage.app",
-
-  messagingSenderId: "664860565094",
-
-  appId: "1:664860565094:web:ec91285b3200b125a017a7",
-
-  measurementId: "G-XHY935VDBE"
-
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
-
 
 
 const app = initializeApp(firebaseConfig);
