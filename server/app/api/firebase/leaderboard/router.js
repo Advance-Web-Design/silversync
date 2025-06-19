3fc1a15d54ad547672dfbc3264c18cb55d244f3d
Michaelbtnr <michael.btnr@gmail.com>
2025-06-19 17:18:44 +0300

    added func to get scores from db. added state to hold leadeboard data

---
+++ server/app/api/firebase/leaderboard/router.js
@@ -0,0 +1,40 @@
+import { NextResponse } from 'next/server';
+import { initializeFirebase } from '@/utils/firebaseAdmin';
+
+export async function GET() {
+  const { db } = initializeFirebase();
+  const usersSnapshot = await db.ref('users').once('value'); //go through all users to check for scores
+  const allUsers = usersSnapshot.val();
+
+  if (!allUsers)
+    return NextResponse.json([], { status: 200 });
+
+  let scores = [];
+  for (const username in allUsers) {
+    const gameHistory = allUsers[username].gamehistory;
+    if (!gameHistory)
+        continue; //game history is not found (no game played yet)
+
+    for (const mode in gameHistory) {
+/* TODO: make sure leaderboard shows different game modes*/
+      gameHistory[mode].forEach(entry => {
+        scores.push({
+          username,
+          score: entry.score,
+          time: entry.timeTaken,
+        });
+      });
+    }
+  }
+
+  // Sort by score (lower is better)
+  scores.sort((a, b) => {
+    if (a.score === b.score) {
+      return a.time.localeCompare(b.time);
+    }
+    return a.score - b.score;
+  });
+
+  const top10 = scores.slice(0, 10); //show only top 10 scores
+  return NextResponse.json(top10);
+}
